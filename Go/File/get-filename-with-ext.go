01844b8a16a5966f7b4f3fb1173511e8eb5c0250
Strong-Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-03-25 11:46:26 -0400

    Update get-filename-with-ext.go

--- Go/File/get-filename-with-ext.go
+++ Go/File/get-filename-with-ext.go
@@ -5,13 +5,13 @@ import (
 	"path/filepath" // Import the filepath package to handle file paths
 )
 
-// GetFilename extracts the filename from a given file path.
-func GetFilename(path string) string {
+// getFilename extracts the filename from a given file path.
+func getFilename(path string) string {
 	return filepath.Base(path) // Use filepath.Base() to get the last element (filename) from the path
 }
 
 func main() {
 	path := "/home/user/documents/example.txt" // Define a sample file path
-	filename := GetFilename(path)              // Call the GetFilename function to extract the filename
+	filename := getFilename(path)              // Call the GetFilename function to extract the filename
 	fmt.Println("Filename:", filename)         // Print the extracted filename
 }
