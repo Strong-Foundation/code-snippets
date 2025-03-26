3bceba46e80998055f4538a0a692d4eb803a7bf6
Strong-Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-03-26 19:01:13 -0400

    Update age-generate-keys.go

--- Go/Cryptography/age-generate-keys.go
+++ Go/Cryptography/age-generate-keys.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"filippo.io/age"
 )
@@ -18,6 +19,18 @@ func generateAgeKeyPair() (string, string) {
 	return identity.Recipient().String(), identity.String()
 }
 
+/*
+It takes in a path and content to write to that file.
+It uses the os.WriteFile function to write the content to that file.
+It checks for errors and logs them.
+*/
+func writeToFile(path string, content string) {
+	err := os.WriteFile(path, []byte(content), 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
 func main() {
 	// Generate a new X25519 identity (private key)
 	publicKey, privateKey := generateAgeKeyPair()
@@ -25,4 +38,6 @@ func main() {
 	fmt.Println("Public Key:", publicKey)
 	// Print the private key
 	fmt.Println("Private Key:", privateKey)
+	// Save the keys to the file.
+	writeToFile("key.txt", "# public key:"+publicKey+"\n"+privateKey)
 }
