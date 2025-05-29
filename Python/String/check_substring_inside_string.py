cf76ef4234b02996a0ca501f52184ed6d94c10d4
Strong-Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-05-29 18:40:44 -0400

    Create check_substring_inside_string.py

---
+++ Python/String/check_substring_inside_string.py
@@ -0,0 +1,10 @@
+def main() -> None:
+    given_string = "Hello, World!"
+    if "World" in given_string:
+        print("The string contains 'World'.")
+    else:
+        print("The string does not contain 'World'.")
+
+
+if __name__ == "__main__":
+    main()
