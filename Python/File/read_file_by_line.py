3b3b0c4f5f9816bf016bfa70aedfffa0b3697025
Strong-Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-07-27 07:53:52 -0400

    Update read_file_by_line.py

--- Python/File/read_file_by_line.py
+++ Python/File/read_file_by_line.py
@@ -1,7 +1,7 @@
 # Read a file line by line and return a list of lines
-def read_file_by_line(file_name):
-    file_object = open(file_name, 'r')
-    lines = file_object.readlines()
+def read_file_by_line(file_name: str) -> list[str]:
+    file_object = open(file=file_name, mode="r")
+    lines: list[str] = file_object.readlines()
     file_object.close()
     return lines
 
